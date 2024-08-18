f428d5900e167ff21f55709c7f4b382bf098a920
Maxime Filippini <maxime.filippini@gmail.com>
2024-08-18 22:09:01 +0200

    update paths for build2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./priv/site/**/*.{html,js}"],
+  content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
